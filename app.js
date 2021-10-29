67cb3340928e9848b0c683a514dd23f11412d0e0
Shrreya <shrreya@re-coded.com>
2021-10-30 01:47:37 +0530

    remove solution code

--- app.js
+++ app.js
@@ -1,66 +1,26 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const app = express()
-const {
-    check,
-    validationResult
-} = require("express-validator")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(express.json())
-app.set("view engine", "ejs")
-app.set("views", "views")
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const usedEmails = [
-    "halit@re-coded.com",
-    "shrreya@re-coded.com",
-    "ammar@re-coded.com",
-    "osama@re-coded.com",
-    "muhannad@re-coded.com"
+  "halit@re-coded.com",
+  "shrreya@re-coded.com",
+  "ammar@re-coded.com",
+  "osama@re-coded.com",
+  "muhanned@re-coded.com",
 ];
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
-app.post("/users",
-    check('username', 'Username must be atleast 4 characters long').exists().isLength({
-        min: 4
-    }).not().matches(/\s/).withMessage('Username should not include spaces').notEmpty().withMessage('Username should not be empty'),
-    check('email', 'Invalid email').notEmpty().withMessage('Email should not be empty').isEmail().normalizeEmail(),
-    check('email', 'Email already exists').custom((value) => {
-        return !(usedEmails.includes(value));
-    }),
-    check('password', 'The password must be 5+ chars long and contain a number, uppercase and lowercase')
-    .isLength({
-        min: 5
-    })
-    .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/),
-    check('password', 'Passwords are not matching').custom((value, {
-        req
-    }) => {
-        if (value !== req.body.confirmPassword) {
-            return false
-        }
-        return true
-    }),
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-             alerts = errors.array()
-            res.render("index", {
-                alerts
-            })
-        } else {
-            usedEmails.push(req.body.email)
-            res.render("index",{
-                success:"Congratulations, your account has been successfully created"
-            })
-        }
-    })
+app.listen(port, () => console.log(`Application running on port ${port}`));
 
-app.listen(3000, () => console.log("Server running on port 3000"))
-
-module.exports = app
+module.exports = app;
