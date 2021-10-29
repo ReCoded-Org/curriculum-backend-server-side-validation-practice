8eb212be238ac7ea7acdab1b98670cae2bef2b09
Shrreya <shrreya@re-coded.com>
2021-10-30 01:52:55 +0530

    add solution code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,44 @@ const usedEmails = [
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.post(
+  "/users",
+  check("username", "Username must be at least 4 characters long")
+    .notEmpty()
+    .withMessage("Username should not be empty")
+    .isLength({min: 4})
+    .not()
+    .matches(/\s/)
+    .withMessage("Username should not include spaces"),
+  check("email", "Invalid email")
+    .notEmpty()
+    .withMessage("Email should not be empty")
+    .isEmail()
+    .normalizeEmail()
+    .custom((value) => !usedEmails.includes(value))
+    .withMessage("Email already exists"),
+  check("password", "Password must be at least 5 characters long")
+    .notEmpty()
+    .withMessage("Password should not be empty")
+    .isLength({min: 5})
+    .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/)
+    .withMessage("Password must contain a number, uppercase and lowercase")
+    .custom((value, { req }) => value === req.body.confirmPassword)
+    .withMessage("Passwords are not matching"),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      alerts = errors.array();
+      res.render("index", {alerts});
+    } else {
+      usedEmails.push(req.body.email);
+      res.render("index", {
+        success: "Congratulations, your account has been successfully created!",
+      });
+    }
+  }
+);
 
 app.listen(port, () => console.log(`Application running on port ${port}`));
 
